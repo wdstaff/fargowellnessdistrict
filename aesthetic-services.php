40a5ce723914d581d905aebb9eb856a4b0446e16
wdstaff <86987478+wdstaff@users.noreply.github.com>
2021-12-08 00:09:28 -0600

    Add files via upload

---
+++ aesthetic-services.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<head>
+
+  <title>Aesthetic Services at Wellness District</title>
+  <meta name="description" content="Fargo Wellness District Specializes In Services Like Botox, Coolsculpting, Medical Weight Loss, Juvederm And Kybella!">
+  <meta name="viewport" width="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/aesthetic-services.css" />
+  <!-- Lato Font Link -->
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
+</head>
+<body>
+
+  <?php include 'wdheader2.php' ?>
+  <main>
+      <img class="img-header" src="pics/services-banner.jpg" />
+
+      <section class="first section w3-container w3-center w3-animate-bottom">
+        <h1 class="section-heading-text">THE WELLNESS DISTRICT WAY</h1>
+        <p class="section-p-text">No two faces are the same which is why we believe that everyone deserves a personalized, one-of-a-kind treatment plan. We specialize in the most effective and safe FDA-approved, natural-looking, non-surgical medical facial injectables and skin rejuvenation treatments.</p>
+      </section>
+
+      <section class="second section">
+        <div class="second-section-item item1"><img class="pink-pic" src="pics/icon1.png"><h2 class="item-header">Expert Advice</h2><p class="item-p">Take care of your skin 24/7 with a customized regimen catered to your needs.</p></div>
+        <div class="second-section-item item2"><img class="pink-pic" src="pics/icon2.png"><h2 class="item-header">Personalized Treatments</h2><p class="item-p">Improve your skin’s tone & texture with microneedling, PRF & chemical peels.</p></div>
+        <div class="second-section-item item3"><img class="pink-pic" src="pics/icon3.png"><h2 class="item-header">Customized Regimens</h2><p class="item-p">Take care of your skin 24/7 with a customized regimen catered to your needs.</p></div>
+      </section>
+
+      <div class="funky-background">
+
+        <h3 class="all-treatments-section-header">We focus on your areas of concern</h3>
+
+        <section class="ind-treatment-section">
+          <div class="left-ind-treatment-section-wrapper">
+              <img class="ind-treatment-section-img" src="pics/slideshow/botox-mobile3.jpg">
+          </div>
+          <div class="right-ind-treatment-section-wrapper">
+              <h3 class="right-ind-treatment-section-header">Fine lines & wrinkles</h3>
+              <p class="right-ind-treatment-section-text">If you want a more lifted eye area or to soften fine lines and wrinkles, ask your Injection Specialist about wrinkle relaxers! When used strategically, wrinkle relaxers can slightly lift the forehead muscle and temporarily smooth crow’s feet, frown lines and other wrinkles caused by repetitive facial movements including neck lines.</p>
+              <a class="ind-treatment-section-button" href="#">Meet the Providers</a>
+          </div>
+        </section>
+
+
+
+        <section class="ind-treatment-section ind-treatment-section-off">
+          <div class="right-ind-treatment-section-wrapper">
+              <h3 class="right-ind-treatment-section-header">Dermal Fillers</h3>
+              <p class="right-ind-treatment-section-text">If you want a more lifted eye area or to soften fine lines and wrinkles, ask your Injection Specialist about wrinkle relaxers! When used strategically, wrinkle relaxers can slightly lift the forehead muscle and temporarily smooth crow’s feet, frown lines and other wrinkles caused by repetitive facial movements including neck lines.</p>
+              <a class="ind-treatment-section-button" href="#">Meet the Providers</a>
+          </div>
+          <div class="left-ind-treatment-section-wrapper">
+              <img class="ind-treatment-section-img" src="pics/pout-square.jpg">
+          </div>
+        </section>
+
+
+
+        <section class="ind-treatment-section">
+          <div class="left-ind-treatment-section-wrapper">
+              <img class="ind-treatment-section-img" src="pics/coolsculpting-treatement-pic.jpg">
+          </div>
+          <div class="right-ind-treatment-section-wrapper">
+              <h3 class="right-ind-treatment-section-header">Coolsculpting</h3>
+              <p class="right-ind-treatment-section-text">If you want a more lifted eye area or to soften fine lines and wrinkles, ask your Injection Specialist about wrinkle relaxers! When used strategically, wrinkle relaxers can slightly lift the forehead muscle and temporarily smooth crow’s feet, frown lines and other wrinkles caused by repetitive facial movements including neck lines.</p>
+              <a class="ind-treatment-section-button" href="#">Meet the Providers</a>
+          </div>
+        </section>
+
+        <section class="ind-treatment-section ind-treatment-section-off">
+          <div class="right-ind-treatment-section-wrapper">
+              <h3 class="right-ind-treatment-section-header">Medical Weight Loss</h3>
+              <p class="right-ind-treatment-section-text">If you want a more lifted eye area or to soften fine lines and wrinkles, ask your Injection Specialist about wrinkle relaxers! When used strategically, wrinkle relaxers can slightly lift the forehead muscle and temporarily smooth crow’s feet, frown lines and other wrinkles caused by repetitive facial movements including neck lines.</p>
+              <a class="ind-treatment-section-button" href="#">Meet the Providers</a>
+          </div>
+          <div class="left-ind-treatment-section-wrapper">
+              <img class="ind-treatment-section-img" src="pics/mwlspic1.jpg">
+          </div>
+        </section>
+
+
+      </div>
+
+
+
+
+
+      <section class="carousel-section">
+        <h3 class="carousel-section-header">Meet our amazing team of aesthetic providers</h3>
+        <a class="carousel-section-button" href="#">Meet the Providers</a>
+
+      </section>
+      <div class="carouselins"><?php include 'carousel.php' ?></div>
+
+  </main>
+
+</body>
+<?php include 'wdfooter2.php' ?>
+</html>
