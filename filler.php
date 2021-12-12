1ddf6ed1309697a87291f0f3643a7508a93eb2d7
wdstaff <86987478+wdstaff@users.noreply.github.com>
2021-12-12 17:07:05 -0600

    b

--- filler.php
+++ filler.php
@@ -10,89 +10,175 @@
 </head>
 
 <body>
-
   <main>
-<?php include 'wdheader.php' ?>
+  <?php include 'wdheader.php' ?>
 
-    <h1 class="page_header"><a>DERMAL FILLER</a></h1>
-    <p class="paragraph_header">At Wellness District Aesthetics of Fargo, we use advanced techniques like the blunt-tip cannula when injecting our dermal filler. The <b><a href="advanced-techniques.php#blunt-tip">BLUNT-TIP MICRO CANNULA </a></b> can help improve the way dermal filler is injected into the skin. The micro cannula is a small, flexible needle with a blunt-tip used to minimize discomfort and bruising when injecting dermal filler. This process has also allowed our patients to return back to work or normal daily activities with little to no downtime. Some other benefits of this technique include fewer injection sites, less risk of complications, the ability to cover larger treatment areas and the utilization of less product.
-    </p>
+  <img class="img-header" src="pics/slideshow/botox-mobile3.jpg" />
+
+  <section class="first-section">
+
+    <div class="left-section">
+      <div class="left-wrapper">
+        <h1 class="page-header">Cheek Enhancement & Revolumization</h1>
+
+        <p class="section-text">The goal of cheek enhancement with dermal fillers is to add volume or lift the cheeks. As we age, we gradually lose facial volume. Our mid face and lower face is usually where this is seen most with “jowls”, sagginess, and deep nasolabial folds sometimes appearing. By injecting a hyaluronic acid based dermal filler such as Restylane Contour, Restylane Lyft, Juvederm Voluma, or Radiesse along the cheekbones and in the temples, we can help lift the entire lower face. The results are diminished jowling and softer nasolabial folds and a revolumization of the cheek area. This area can also be treated with Sculptra® Aesthetic, a poly-l-lactic acid that helps stimulate your skin’s own natural collagen production over time.
+        </p>
+
+    </div>
+  </div>
+
+    <div class="right-section">
+      <div class="right-wrapper container-spec">
+      <h3 class="heading-text">Recommended Treatments:</h3>
+      <ol class="treatments">
+        <li>Chemical Peels</li>
+        <li>Microneedling</li>
+        <li>Wrinkle Relaxers</li>
+        <li>Platlet-Rich Fibrin (PRF)
+        </ol>
+        <hr>
+        <h3 class="heading-text">Skincare:</h3>
+        <ol class="skincare">
+          <li>C+ Correcting Complex 30%</li>
+          <li>Restorative Complex</li>
+          <li>TNS Advanced+</li>
+        </ol>
+        <hr>
+        <h3 class="heading-text">Treatment areas:</h3>
+        <ol class="treatment-area">
+            <li>Bunny Lines</li>
+            <li>Crow's Feet</li>
+            <li>Forehead Lines</li>
+            <li>Frown Lift</li>
+            <li>Glabella/Frown Lines</li>
+            <li>Gummy Smile (overexposure of the upper teeth & gums when smiling)</li>
+            <li>Hyperhidrosis (excessive sweating in underarms or hands)</li>
+            <li>Mentalis (dimpled chin)</li>
+            <li>Marionette Lines</li>
+            <li>Platysma Bands - Aging muscle neck lines/bands</li>
+            </ol>
 
 
-    <section class="section white">
-      <img class="sectionimg smallimg" src="pics/pout-square.jpg" alt="Dermal Filler Lips">
-      <h2 class="subheading">LIP PACKAGES</h2>
-      <h2 class="listheader">Petite Pout: Pricing at your FREE consultation</h2>
-          <li>For clients looking for a small but powerful lip enhancement, this is the treatment for you! This is also great for patients that are new to Dermal Fillers.</li>
-      <br>
-      <br>
-      <h2 class="listheader">Perfect Pout: Pricing at your FREE consultation</h2>
-      <li>​Get the lips you have always wanted. This lip enhancement will give you the perfect pout while still leaving you looking natural and beautiful!</li>
-      <br>
-      <br>
-      <h2 class="listheader">Pillow Pout: Pricing at your FREE consultation</h2>
-      <li>​Get dramatic lips that give you all the attention. You can now have movie star lips in Fargo!</li>
-      <br>
-      <br>
-      <h2 class="listheader">Bye-Bye Lipo Lines: Pricing at your FREE consultation</h2>
-      <li>​​Smooth out those lines above your lip. This procedure alone can take years off your look. This is great in combination with our petite, perfect, or pillow pout.</li>
-      <br>
-      <br>
-      <a href="filler-book.php" class="button">SEE BEFORE & AFTERS HERE</></a>
+
+    </div>
+  </div>
+
+
+  </section>
+
+  <section class="second-section">
+    <div class="second-left-section">
+      <div class="second-left-wrapper">
+        <h1 class="second-page-header">Real Botox Results</h1>
+
+        <p class="section-text">Our goal has remained the same since we opened our doors back in 2009— natural-looking, gorgeously refreshed patient results.</p>
+        <p class="section-text">This beauty received wrinkle relaxers to smooth & relax fine lines & wrinkles around the crow’s feet, forehead & frown.</p>
+        <p class="section-text">You’ll also notice her entire eye area is now more open & ‘awake’— our expert Injection Specialists use a ‘brow lift’ technique to achieve this look.</p>
+
+    </div>
+  </div>
+  <div class="second-right-section">
+    <div class="second-right-wrapper">
+      <h4 class="above-img-text">BEFORE • AFTER</h3>
+      <img class="right-section-img" src="pics/botoxbf1.jpg">
+      <h4 class="under-img-text">Results may vary</h4>
+    </div>
+  </div>
+
+  </section>
+
+  <div class="funky-background">
+  <section class="testimonials-section">
+      <iframe class="testiholder" loading="lazy" title="Client Reviews for Fargo Wellness District's aesthetic treatments and spa services" src="https://app.getsmiles.com/Testimonials.aspx?pid=812830&amp;format=1" width='90%' height="400px"></iframe>
+
+  </section>
+  <a class="appt-button testbutton testbuttonlrg" href="#">Book an appointment</a>
+</div>
+  <section class="third-section faq-section">
+              <button class="accordion">What is BOTOX Cosmetic?</button>
+          <div class="panel">
+            <p>BOTOX Cosmetic is an injectable treatment made from botulinum toxin type A, a substance which temporarily relaxes the muscles responsible for causing lines and wrinkles to form. When BOTOX is injected into the face or neck, it relaxes lines and wrinkles as well as tightening sagging skin along the jawline.</p>
+          </div>
+
+          <button class="accordion">Why choose Wellness District Aesthetics among the countless Fargo Moorhead area aesthetics practices?</button>
+          <div class="panel">
+            <p>For starters, our practice is the #1 BOTOX provider in Santa Monica, CA. Nationally, we are ranked in the top 10 for Botox and Juvederm by their manufacturer Allergan.</p>
+            <p>We specialize in injectables, and so our providers administer a higher volume of Botox treatments. In fact, we inject more in two weeks than most high performing dermatologists and plastic surgeons do in an entire year! Additionally, several of our Aesthetic Injection Specialists are National Trainers for Allergan. Our industry accolades are mirrored in our five star reviews and real patient results, clearly demonstrating why they continue to see us instead of choosing one of countless competing aesthetics practices across Los Angeles.</p>
+          </div>
+
+          <button class="accordion">Are there ways to save on my BOTOX Cosmetic treatment?</button>
+          <div class="panel">
+            <p>Yes, we accept Alle rewards! It’s free to join and you’ll earn points on every BOTOX Cosmetic, Kybella, or Juvederm treatments you receive! You will also receive points for every internal Latisse and SkinMedica purchase you make. Best of all, it’s free to join. You’ll be able to redeem points for future savings.</p>
+          </div>
+
+          <button class="accordion">What about downtime, results and follow-up appointments for a wrinkle relaxer treatment?</button>
+          <div class="panel">
+            <p>The best part about a wrinkle relaxer treatment is that you will have no downtime and should be able to continue your day immediately after receiving your injections. Results vary but are usually visible by day seven. We may recommend a follow-up appointment to evaluate your results and, if necessary, administer a little more treatment.</p>
+            <p>After three or four months, you will need a new treatment and can return to Skin by Lovely to maintain your fresh, youthful look.</p>
+          </div>
+
+          <button class="accordion">What is the pre & post care?</button>
+          <div class="panel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+          <script>
+                    var acc = document.getElementsByClassName("accordion");
+                    var i;
+
+                    for (i = 0; i < acc.length; i++) {
+                    acc[i].addEventListener("click", function() {
+                      this.classList.toggle("active");
+                      var panel = this.nextElementSibling;
+                      if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                      } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                      }
+                    });
+                    }
+                  </script>
+
+    </section>
+    <section class="fourth-section">
+      <h3 class="fourth-header">Meet our amazing team of aesthetic providers</h3>
+      <a class="appt-button testbutton" href="#">Meet the Providers</a>
+
+    </section>
+    <div class="carouselins"><?php include 'carousel.php' ?></div>
+    <section id="new-patient-special" class="botox-special-section">
+
+      <img src="pics/botoximg.png" class="botox-special-section-img">
+      <div class="botox-special-section-text-wrapper">
+          <h4 class="botox-special-section-heading">Botox special</h4>
+            <p class="botox-special-section-text">We are so confident in our techniques and precise placement that we offer patients their first Botox® treatment for $150 (up to 35 units).</p>
+              <ol class="terms-of-service-botox">
+                <li>1 Treatment Of Botox® Up To 35 Units</li>
+                <li>Additional Units $13 Per Unit.</li>
+                <li>Unused Units Will Expire 1 Month After Injection</li>
+                <li>Injection Must Be Done M-TH Between 9am - 3:30pm</li>
+                <li>1st Time Wellness District Patients Only</li>
+
+              </ol>
+
+              <a class="botox-special-section-button " href="https://schedulingapp.mypatientnow.com/Account/Login/719">Book an appointment</a>
+
+
+      </div>
+
+
     </section>
 
 
-    <section class="section black">
-      <img class="sectionimg smallimg" src="pics/fillerimg.jpg" alt="Dermal Filler Juvederm">
-      <h2 class="subheading">LOWER FACE PACKAGES</h2>
-      <h2 class="listheader">Smooth Operator: Pricing at your FREE consultation</h2>
-          <li>Wanting to erase the most frustrating signs of aging? The smooth operator treatment gets rid of the most annoying lines around your mouth.</li>
-      <br>
-      <br>
-      <h2 class="listheader">Smoothest Operator: Pricing at your FREE consultation</h2>
-      <li>​This is the Smooth Operator treatment but with extra product in order to make sure you get the amazing results you are looking for. At your free consultation our expert injectors can let you know which package they think would be right for you.</li>
-      <br>
-      <br>
-      <h2 class="listheader">Pillow Pout: Pricing at your FREE consultation</h2>
-      <li>​Get dramatic lips that give you all the attention. You can now have movie star lips in Fargo!</li>
-      <br>
-      <br>
-      <a href="filler-book.php" class="button">SEE BEFORE & AFTERS HERE</></a>
+    <section class="fifth-section">
+      <img class="fifth-section-img" src="pics/wdtreatment.jpg">
+      <div class="fifth-section-color-block"></div>
+      <div class="sneaky-img-container">
+      <p class="sneaky-img-text">View all our treatments</p>
+      <a class="sneaky-img-button-left appt-button" href="#">Learn more</a>
+      <a class="sneaky-img-button-right appt-button" href="#">Book online</a>
+    </div>
     </section>
-
-
-    <section class="section black rev bottom">
-      <img class="sectionimg smallimg" src="pics/vollure-model.jpg" alt="Dermal Filler Model">
-      <h2 class="subheading"> MID AND UPPER FACE PACKAGES:</h2>
-      <h2 class="listheader">Contour Packages: Pricing at your FREE consultation</h2>
-          <li>Cheeks, Chins, Jawlines and More!!! Let us redefine your age with one of our exclusive facial enhancements.</li>
-      <br>
-      <br>
-      <h2 class="listheader">Contour and Define: Pricing at your FREE consultation</h2>
-      <br>
-      <br>
-      <h2 class="listheader">Contour, Define, and Sculpt: Pricing at your FREE consultation</h2>
-      <br>
-      <br>
-      <h2 class="listheader">Contour Signature Enhancement: Pricing at your FREE consultation</h2>
-      <br>
-      <br>
-      <h2 class="listheader">Check Your Bags: Pricing at your FREE consultation</h2>
-          <li>Under eye bags are one of the biggest concerns we hear about in our Aesthetic Practice. They can leave you looking tired and aged. Check Your Bags can help to diminish the signs of under eye bags. This is a great treatment to pair with our Contour packages.</li>
-
-      <br>
-      <br>
-      <h2 class="listheader">Temple of Youth: Pricing at your FREE consultation</h2>
-          <li>Often overlooked, loss of volume in the temples can lead to a tired, aged appearance. Restore your youthful fullness with the Temple of Youth. You won't believe your results!</li>
-
-      <br>
-      <br>
-      <a href="https://schedulingapp.mypatientnow.com/Account/Login/719" class="button">SCHEDULE YOUR APPOINTMENT HERE</></a>
-    </section>
-
   </main>
-    <?php include 'wdfooter.php' ?>
 </body>
-
-
+<?php include 'wdfooter.php' ?>
 </html>
